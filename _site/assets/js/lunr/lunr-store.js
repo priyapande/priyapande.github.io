5bde5d13c68b2444577038df1849880519f6784c
priyapande <priyapande07@gmail.com>
2020-07-10 23:22:19 +0530

    adding _site folder to enable tags

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,13 @@
+var store = [{
+        "title": "Sharing State in React Using EventEmitter",
+        "excerpt":"How do React Components share information among components? React libraries do not offer any global state store. Then how do we share information between components? In a parent-child relationship, we pass the parent’s state as props to child. For example, in an MCQ exam portal we may have components like...","categories": ["frontend"],
+        "tags": ["react","eventemitter","javascript"],
+        "url": "http://localhost:4000/frontend/eventemitter",
+        "teaser": null
+      },{
+        "title": "Understanding Permissions in Docker Containers",
+        "excerpt":"Many of us may have heard about Docker and containers being used in microservices architecture. The main advantage that containerization gives us is that many small apps can run as light-weight containers on the same OS and share resources. But, it is also important to understand how UIDs and GIDs...","categories": ["devops","deployment"],
+        "tags": ["docker","shell","openshift"],
+        "url": "http://localhost:4000/devops/deployment/containerpermissions",
+        "teaser": null
+      }]
